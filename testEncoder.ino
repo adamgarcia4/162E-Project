0bc601855592a60ecfc6dc242fe61962421feef4
Adam Garcia <adamgarcia1994@ucla.edu>
2017-05-15 17:57:12 -0700

    use this code

--- testEncoder.ino
+++ testEncoder.ino
@@ -3,17 +3,19 @@
 // #include "SynchPID.h"
 #include <PID_v1.h>
 #include <stdio.h>
-#include "Autotune.h"
+//#include "Autotune.h"
 
 Controller* m_controller1;
 //Controller* m_controller2;
 IMU* m_imu;
 unsigned long m_currTime;
 volatile int m_numEncoder;
+volatile long m_microsTime;
+volatile long m_lastMicrosTime;
 float angleArr[3];
 double input, output, setpoint;
 
-PID PID1(&input, &output, &setpoint, 0.022, 0.000,0.00, DIRECT); //0.008, .8,0.00001
+PID PID1(&input, &output, &setpoint, 0.0022, 0.000,0.00, DIRECT); //0.008, .8,0.00001 | .022
 //.015
 
 void setup(){
@@ -32,6 +34,7 @@ void setup(){
   PID1.SetMode(AUTOMATIC);
   PID1.SetOutputLimits(0,0.8);
   PID1.SetSampleTime(200);
+//  input = 0;
 
   // Final Setup Delay
   delay(20);
@@ -39,34 +42,38 @@ void setup(){
 
 void loop(){
 
-    m_controller1->updateTickCount(m_numEncoder);
-    m_numEncoder = 0;
+   m_controller1->updateTickCount(m_numEncoder);
+   m_numEncoder = 0;
    m_currTime = millis(); //Update current time
    m_controller1->updateTimer(m_currTime);
 
-    // Routine to print rad/s
+   // for(int i=0;i<100;i++) {
+   //    m_controller1->driveMotor(0.5*i-0.5);
+   //    delay(2000);
+   //    // m_controller1->driveMotor(-1);
+   //    // delay(2000);
+   // }
+//   m_controller1->driveMotor(0.7);
+   
+//   Serial.println(m_controller1->readEncoder());
+   
+   //    Serial.println(m_controller1->getEncoderRate());
+   //
+   //
 
-    for(int i=0;i<100;i++) {
-      m_controller1->driveMotor(1);
-      delay(2000);
-      m_controller1->driveMotor(-1);
-      delay(2000);
-    }
-      m_controller1->driveMotor(0);
-      delay(100000);
-//    m_controller1->driveMotor(0.5);
-//    m_controller2->driveMotor(0.5);
-//    Serial.println(m_controller1->getEncoderRate());
-//
-//
-//  input = m_controller1->getEncoderRate();
-//  PID1.Compute();
-////   Serial.print(input); Serial.print(","); Serial.println(output);
-//   Serial.println(m_controller1->getEncoderRate());
-//   m_controller1->driveMotor(output);
+   //TEST
+  m_controller1->driveMotor(0.09);
+  Serial.println(m_controller1->getEncoderRate());
+   
+//     input = m_controller1->getEncoderRate();
+//     Serial.println(input);
+//     PID1.Compute();
+//      Serial.print(input); Serial.print(","); Serial.println(output);
+//   //   Serial.println(m_controller1->getEncoderRate());
+//      m_controller1->driveMotor(output);
 
 
-   delay(200); //Looping refresh rate (ms)
+   delay(20); //Looping refresh rate (ms)
 }
 
 int convertVal(double input) {
@@ -80,6 +87,9 @@ int convertVal(double input) {
 }
 
 void encoderChange() {
+  m_lastMicrosTime = m_microsTime;
+   m_microsTime = micros();
+   
    m_numEncoder++;
 }
 
